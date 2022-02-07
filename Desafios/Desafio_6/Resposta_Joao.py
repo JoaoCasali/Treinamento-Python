1c9c91c24e51ba7a34a0685cba8fb8f0552dc47f
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2022-02-07 18:49:18 -0300

    Desafio 6

---
+++ Desafios/Desafio_6/Resposta_Joao.py
@@ -0,0 +1,7 @@
+SEMANA = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+
+doses = int(input("Digite o quantas doses deve-se tomar: "))
+intervalo = int(input("Digite o intervalo de tempo entre as doses: "))
+dia = 1
+for i in range(doses):
+    break
