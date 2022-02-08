279b2bb94e07a40251564ffb24ed07be959d5c85
JoaoCasali <82387159+JoaoCasali@users.noreply.github.com>
2022-02-07 23:31:34 -0300

    Mudandças e resoluções D6
    Mudanças no desafio 6 e a resolução dele

--- Desafios/Desafio_6/Resposta_Joao.py
+++ Desafios/Desafio_6/Resposta_Joao.py
@@ -1,7 +1,12 @@
-SEMANA = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+from datetime import date
+SEMANA = ["segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado", "domingo"]
+dia = date.today().weekday()
+doses = int(input("Digite a quantidade de doses que devão ser tomadas: "))
+intervalo = int(input("Digite o intervalo de dias entre as doses: "))
+for d in range(doses):
+    for i in range(intervalo):
+        dia += 1
+        if dia == 7:
+            dia =0
 
-doses = int(input("Digite o quantas doses deve-se tomar: "))
-intervalo = int(input("Digite o intervalo de tempo entre as doses: "))
-dia = 1
-for i in range(doses):
-    break
+print(f"A ultima dose será em um(a) {SEMANA[dia]}")
